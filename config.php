81858943ff70e6f108f799bd3d1bef0683dd433c
unknown <nirav.p@VTDT024.VTECHSOLUTION.COM>
2018-05-11 14:43:21 -0400

    add

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2', //Example https://appcenter.intuit.com/connect/oauth2',
+  'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer', //Example https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+  'client_id' => 'Q0i1fPYLMH28GivZTRPxKrQG4OSkrqzi1KAwMiLWw5yFawwIi8', //Example 'Q0wDe6WVZMzyu1SnNPAdaAgeOAWNidnVRHWYEUyvXVbmZDRUfQ',
+  'client_secret' => 'zADHboOn5CMCqVOZvSNSvhkbry8fHg6OqTDvCIBZ', //Example 'R9IttrvneexLcUZbj3bqpmtsu5uD9p7UxNMorpGd',
+  'oauth_scope' => 'com.intuit.quickbooks.accounting', //Example 'com.intuit.quickbooks.accounting',
+  'openID_scope' => 'openid profile email', //Example 'openid profile email',
+  'oauth_redirect_uri' => 'https://qbdemo1.herokuapp.com/OAuth2PHPExample.php', //Example https://d1eec721.ngrok.io/OAuth_2/OAuth2PHPExample.php',https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl
+  'openID_redirect_uri' => 'https://qbdemo1.herokuapp.com/OAuthOpenIDExample.php',//Example 'https://d1eec721.ngrok.io/OAuth_2/OAuthOpenIDExample.php',
+  'mainPage' => 'https://qbdemo1.herokuapp.com/index.php', //Example https://d1eec721.ngrok.io/OAuth_2/index.php',
+  'refreshTokenPage' => 'https://qbdemo1.herokuapp.com/RefreshToken.php', //Example https://d1eec721.ngrok.io/OAuth_2/RefreshToken.php'
+)
+ ?>
